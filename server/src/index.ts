a7b8e8b9834163f361eeddbe6d36a1c6ac13e898
James Craig <jamescraig@finervision.com>
2022-03-07 16:46:02 +0000

    wip: launch browser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,27 @@
+import * as os from "os";
+import { launch } from "puppeteer-core";
 import database from "./services/database";
 import { socket } from "./services/app";
 import config from "./config/config";
 import scan from "./actions/scan";
+
+const executablePaths: { [key: string]: string } = {
+  darwin: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  linux: "/usr/bin/google-chrome",
+  win32: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+  win64: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+};
+
+const args = [
+  "--disable-web-security",
+  "--remote-debugging-port=9222",
+  "--no-first-run",
+  "--disable-pinch",
+  "--no-default-check",
+  "--overscroll-history-navigation=0",
+  `--app=http://localhost:${config.server.port}`,
+  "--kiosk",
+];
 
 (async () => {
   try {
@@ -12,6 +32,21 @@ import scan from "./actions/scan";
     });
 
     console.log(`Server running at http://localhost:${config.server.port}`);
+
+    const browser = await launch({
+      headless: false,
+      args,
+      executablePath: executablePaths[os.platform()],
+      ignoreDefaultArgs: ["--enable-automation"],
+    });
+
+    const context = browser.defaultBrowserContext();
+    await context.clearPermissionOverrides();
+
+    browser.on("disconnected", async () => {
+      await browser.close();
+      process.exit(0);
+    });
   } catch (err) {
     console.error(err);
     process.exit(1);
