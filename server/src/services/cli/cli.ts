f1b8f89f8656d556bdc1518165dac354961fd773
Enijar <TheEnijar@gmail.com>
2022-03-17 12:21:34 +0000

    fix: remove unused generate win times code

--- server/src/services/cli/cli.ts
+++ server/src/services/cli/cli.ts
@@ -1,6 +1,5 @@
 import reset from "./reset";
 import generateQrCodeImages from "./generate-qr-code-images";
-import generateEncryptedWinTimesCsv from "./generate-encrypted-win-times-csv";
 
 (async () => {
   try {
@@ -11,9 +10,6 @@ import generateEncryptedWinTimesCsv from "./generate-encrypted-win-times-csv";
         break;
       case "generateQrCodeImages":
         await generateQrCodeImages();
-        break;
-      case "generateEncryptedWinTimesCsv":
-        await generateEncryptedWinTimesCsv();
         break;
       default:
         console.error(`No command found "${command}"`);
