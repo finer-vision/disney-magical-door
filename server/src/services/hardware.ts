804a6a74a221aa5ea55895f811d0d38d6951ab82
Enijar <TheEnijar@gmail.com>
2022-04-11 13:34:30 +0100

    fix: relay type NO

--- server/src/services/hardware.ts
+++ server/src/services/hardware.ts
@@ -21,7 +21,7 @@ export default class Hardware {
 
   constructor() {
     this.board.on("ready", () => {
-      this.relay = new Relay(10);
+      this.relay = new Relay({ pin: 10, type: "NO" });
       this.rgb = [new Led(4), new Led(5), new Led(6)];
       this.locked = !this.relay.isOn;
       if (this.locked) {
